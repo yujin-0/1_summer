98fa36523a552d4472abc618b634f28470ec2271
yujin-0 <yujinjin0122@gmail.com>
2024-08-10 02:36:36 +0900

    ing

--- whack_a_mole.js
+++ whack_a_mole.js
@@ -1,14 +1,49 @@
-const square = document.querySelectorAll('.square')
-const mole = document.querySelectorAll('.mole')
-const time = document.querySelectorAll('#time')
-let score = document.querySelectorAll('#score')
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const time = document.querySelector('#time')
+let score = document.querySelector('#score')
 
 let result = 0
+let hitPosition
+let currentTime = 30
+let timerId = null
 
-function randomMole() {
-    square.forEach(className => {
-        className.classList.remove('mole')
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
     })
-    let randomPosition = square[Math.floor(Math.random() * 9)]
-    randomPosition.classList.add('mole')
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
+
+    hitPosition = randomSquare.id
 }
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id === hitPosition) {
+            result += 1
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 650)
+}
+
+moveMole()
+
+function countDown() {
+    currentTime--
+    time.textContent = currentTime
+
+    if (currentTime === 0) {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('GAME OVER! 나의 점수는 ' + result)
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
